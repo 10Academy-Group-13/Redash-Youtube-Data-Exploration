dad360be5403c354faa2831a5136260f33b0f1ed
teddycheru <tedc21@yahoo.com>
2024-05-08 18:06:27 +0300

    Add primary and foreign keys to the database schema

--- database/schema.sql
+++ database/schema.sql
@@ -4,75 +4,11 @@ CREATE DATABASE youtube_data;
 -- Switch to the youtube_data database
 USE youtube_data;
 
--- Create the chart_data table for each dataset zip
-CREATE TABLE cities_chart_data (
-    Date TEXT,
-    Cities TEXT,
-    CityName TEXT,
-    Views INTEGER
-);
+-- Create the tables and relationships with primary and foreign keys
 
-CREATE TABLE content_type_chart_data (
-    Date TEXT,
-    Cities TEXT,
-    CityName TEXT,
-    Views INTEGER
-);
-
-CREATE TABLE device_type_chart_data (
-    Date TEXT,
-    DeviceType TEXT,
-    Views INTEGER
-);
-
-CREATE TABLE geography_chart_data (
-    Date TEXT,
-    Geography TEXT,
-    Views INTEGER
-);
-
-
-CREATE TABLE New_and_returning_viewers_chart_data (
-    Date TEXT,
-    NewAndReturningViewers TEXT,
-    Views INTEGER
-);
-
-CREATE TABLE operating_system_chart_data (
-    Date TEXT,
-    OperatingSystem TEXT,
-    Views INTEGER
-);
-
-CREATE TABLE sharing_service_chart_data (
-    Date TEXT,
-    SharingService TEXT,
-    Shares INTEGER
-);
-
-CREATE TABLE subscription_source_chart_data (
-    Date TEXT,
-    SubscriptionSource TEXT,
-    Subscribers INTEGER
-);
-
-CREATE TABLE subscription_status_chart_data (
-    Date TEXT,
-    SubscriptionStatus TEXT,
-    Views INTEGER
-);
-
-
-CREATE TABLE traffic_source_chart_data (
-    Date TEXT,
-    TrafficSource TEXT,
-    Views INTEGER
-);
-
--- Create the table_data table for each dataset zip
 CREATE TABLE cities_table_data (
     Cities TEXT,
-    CityName TEXT,
+    CityName TEXT PRIMARY KEY,
     Geography TEXT,
     Geography1 TEXT,
     Views INTEGER,
@@ -80,75 +16,141 @@ CREATE TABLE cities_table_data (
     AverageViewDuration TEXT
 );
 
+CREATE TABLE cities_chart_data (
+    Date TEXT,
+    Cities TEXT,
+    CityName TEXT,
+    Views INTEGER,
+    FOREIGN KEY (CityName) REFERENCES cities_table_data (CityName)
+);
+
 CREATE TABLE content_type_table_data (
-    ContentType TEXT,
+    ContentType TEXT PRIMARY KEY,
     Views INTEGER,
     WatchTimeHours FLOAT,
     AverageViewDuration TEXT
+);
+
+CREATE TABLE content_type_chart_data (
+    Date TEXT,
+    ContentType TEXT,
+    Views INTEGER,
+    FOREIGN KEY (ContentType) REFERENCES content_type_table_data (ContentType)
 );
 
 CREATE TABLE device_type_table_data (
-    DeviceType TEXT,
+    DeviceType TEXT PRIMARY KEY,
     Views INTEGER,
     WatchTimeHours FLOAT,
     AverageViewDuration TEXT
+);
+
+CREATE TABLE device_type_chart_data (
+    Date TEXT,
+    DeviceType TEXT,
+    Views INTEGER,
+    FOREIGN KEY (DeviceType) REFERENCES device_type_table_data (DeviceType)
 );
 
 CREATE TABLE geography_table_data (
-    Geography TEXT,
+    Geography TEXT PRIMARY KEY,
     Views INTEGER,
     WatchTimeHours FLOAT,
     AverageViewDuration TEXT
+);
+
+CREATE TABLE geography_chart_data (
+    Date TEXT,
+    Geography TEXT,
+    Views INTEGER,
+    FOREIGN KEY (Geography) REFERENCES geography_table_data (Geography)
 );
 
 CREATE TABLE new_and_returning_viewers_table_data (
-    NewAndReturningViewers TEXT,
+    NewAndReturningViewers TEXT PRIMARY KEY,
     Views INTEGER,
     WatchTimeHours FLOAT,
     AverageViewDuration TEXT
+);
+
+CREATE TABLE new_and_returning_viewers_chart_data (
+    Date TEXT,
+    NewAndReturningViewers TEXT,
+    Views INTEGER,
+    FOREIGN KEY (NewAndReturningViewers) REFERENCES new_and_returning_viewers_table_data (NewAndReturningViewers)
 );
 
 CREATE TABLE operating_system_table_data (
-    OperatingSystem TEXT,
+    OperatingSystem TEXT PRIMARY KEY,
     Views INTEGER,
     WatchTimeHours FLOAT,
     AverageViewDuration TEXT
 );
 
+CREATE TABLE operating_system_chart_data (
+    Date TEXT,
+    OperatingSystem TEXT,
+    Views INTEGER,
+    FOREIGN KEY (OperatingSystem) REFERENCES operating_system_table_data (OperatingSystem)
+);
+
 CREATE TABLE sharing_service_table_data (
-    SharingService TEXT,
+    SharingService TEXT PRIMARY KEY,
     Shares INTEGER
 );
 
+CREATE TABLE sharing_service_chart_data (
+    Date TEXT,
+    SharingService TEXT,
+    Shares INTEGER,
+    FOREIGN KEY (SharingService) REFERENCES sharing_service_table_data (SharingService)
+);
+
 CREATE TABLE subscription_source_table_data (
-    SubscriptionSource TEXT,
+    SubscriptionSource TEXT PRIMARY KEY,
     Subscribers INTEGER,
     SubscribersGained INTEGER,
     SubscribersLost INTEGER
 );
 
+CREATE TABLE subscription_source_chart_data (
+    Date TEXT,
+    SubscriptionSource TEXT,
+    Subscribers INTEGER,
+    FOREIGN KEY (SubscriptionSource) REFERENCES subscription_source_table_data (SubscriptionSource)
+);
+
 CREATE TABLE subscription_status_table_data (
-    SubscriptionStatus TEXT,
+    SubscriptionStatus TEXT PRIMARY KEY,
     Views INTEGER,
     WatchTimeHours FLOAT,
     AverageViewDuration TEXT
 );
 
-CREATE TABLE subtitles_and_cc_table_data (
-    SubtitlesAndCC TEXT,
+CREATE TABLE subscription_status_chart_data (
+    Date TEXT,
+    SubscriptionStatus TEXT,
     Views INTEGER,
-    WatchTimeHours FLOAT,
-    AverageViewDuration TEXT
+    FOREIGN KEY (SubscriptionStatus) REFERENCES subscription_status_table_data (SubscriptionStatus)
 );
 
 CREATE TABLE traffic_source_table_data (
-    TrafficSource TEXT,
+    TrafficSource TEXT PRIMARY KEY,
     Views FLOAT,
     WatchTimeHours FLOAT,
     AverageViewDuration TEXT,
     Impressions FLOAT,
     ImpressionsClickThroughRate FLOAT
 );
+
+CREATE TABLE traffic_source_chart_data (
+    Date TEXT,
+    TrafficSource TEXT,
+    Views INTEGER,
+    FOREIGN KEY (TrafficSource) REFERENCES traffic_source_table_data (TrafficSource)
+);
+
+-- Create tables for data that only have table data
 
 CREATE TABLE viewer_age_table_data (
     ViewerAge TEXT,
